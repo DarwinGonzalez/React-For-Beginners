0da873883caf07340612bcfc1d7838c0bd5d550f
Darwin Gonzalez <alu0100812794@ull.edu.es>
2018-12-23 15:30:27 +0100

    First three videos out

---
+++ build/precache-manifest.22d3e78f3a52dbc00d88175c928defd5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8e1fc11dcb6233523c9a",
+    "url": "/static/js/main.8e1fc11d.chunk.js"
+  },
+  {
+    "revision": "1781e9496b5470db50a4",
+    "url": "/static/js/1.1781e949.chunk.js"
+  },
+  {
+    "revision": "831f39d6b0b4f6212b6f0b30b5b0af20",
+    "url": "/index.html"
+  }
+];
