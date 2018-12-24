7f0e4fb1b36d2808fd5576c12a2f25a9998f3560
Darwin Gonzalez <alu0100812794@ull.edu.es>
2018-12-24 13:14:58 +0100

    Video 8 done

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,3 +20,20 @@ class Header extends React.Component {
   }
 }
 export default Header;
+
+// //Example Stateless Functional Component
+// const Header = props => (
+//   <header className="top">
+//     <h1>
+//       Catch
+//       <span className="ofThe">
+//         <span className="of">Of</span>
+//         <span className="the">The</span>he
+//       </span>
+//       Day
+//     </h1>
+//     <h3 className="tagline">
+//       <span>{props.tagline}</span>
+//     </h3>
+//   </header>
+// );
