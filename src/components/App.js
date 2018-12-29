747346b3c98616c3ef72741e2898746ab0420af5
Darwin Gonzalez <alu0100812794@ull.edu.es>
2018-12-29 19:21:23 +0100

    Video 19 (Persisting our State with localStorage) done

--- src/components/App.js
+++ src/components/App.js
@@ -14,10 +14,20 @@ class App extends React.Component {
 
   componentDidMount() {
     const { params } = this.props.match;
+    //First reinstate our localStorage
+    const localStorageRef = localStorage.getItem(params.storeId);
+    if (localStorageRef) {
+      this.setState({ order: JSON.parse(localStorageRef) });
+    }
     this.ref = base.syncState(`${params.storeId}/fishes`, {
       context: this,
       state: "fishes"
     });
+  }
+
+  componentDidUpdate() {
+    const { params } = this.props.match;
+    localStorage.setItem(params.storeId, JSON.stringify(this.state.order));
   }
 
   componentWillUnmount() {
